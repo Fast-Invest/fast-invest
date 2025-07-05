4069b5b7c8976d99f4931065c00eb8d95cd5e601
JuanCanle <amykappa570@gmail.com>
2025-07-05 00:35:27 -0300

    front

---
+++ frontend/src/components/menuWelcome.jsx
@@ -0,0 +1,23 @@
+export default function MenuWelcome() {
+  return (
+    <>
+      <h1 className="text-5xl font-black text-white tracking-tight">
+        Bem-vindo,{" "}
+        <span className="relative inline-block">
+          {/* Sombra com gradiente e blur */}
+          <span
+            className="absolute inset-0 text-transparent bg-gradient-to-r from-lime-400 to-secondary 
+                bg-clip-text blur-md opacity-60 pointer-events-none"
+          >
+            Usuário!
+          </span>
+
+          {/* Texto principal com gradiente visível */}
+          <span className="bg-gradient-to-r from-lime-400 to-secondary text-transparent bg-clip-text">
+            Usuário!
+          </span>
+        </span>
+      </h1>
+    </>
+  );
+}
