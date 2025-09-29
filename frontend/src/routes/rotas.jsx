5386964712ce75799f64f676a5908e0d2ec413dd
JuanCanle <amykappa570@gmail.com>
2025-09-29 19:33:20 -0300

    Feat: Criei a maioria das páginas que seram utilizadas e já coloquei rota para todas

--- frontend/src/routes/rotas.jsx
+++ frontend/src/routes/rotas.jsx
@@ -3,14 +3,23 @@ import Login from "../pages/Login.jsx";
 import Home from "../pages/Home.jsx";
 import ForgotPassword from "../pages/ForgotPassword.jsx";
 import Quotation from "../pages/Quotation.jsx";
+import Wallet from "../pages/Wallet.jsx";
+import Graph from "../pages/Graph.jsx";
+import Config from "../pages/Config.jsx";
+import Info from "../pages/Info.jsx";
 
 export default function Rotas() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
       <Route path="/login" element={<Login />} />
       <Route path="/esqueceusenha" element={<ForgotPassword />} />
-      <Route path="/cotacoes" element={<Quotation />} />
+
+      <Route path="/carteira" element={<Wallet />} />
+      <Route path="/grafico" element={<Graph />} />
+      <Route path="/configuracoes" element={<Config />} />
+      <Route path="/sobre" element={<Info />} />
     </Routes>
   );
 }
